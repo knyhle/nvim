31a515b66d343a5f50b928cc937100f99c4b2b0e
Kenny Le <kennyle1412@gmail.com>
2024-02-18 14:18:23 -0600

    feat: Enhance CopilotChat and navigation functionality
    - Update CopilotChat to new version and enhance its functionality with new prompts and actions.
    - Add new keybindings for CopilotChat.
    - Update harpoon to new branch and add new keybindings for file navigation.
    - Add new function to jump and center on mark.
    - Add new function to save and center.
    - Add new keybindings for scrolling.
    - Set python3_host_prog in options.
    - Update tmux navigation to disable when zoomed.

--- lua/plugins/tmux.lua
+++ lua/plugins/tmux.lua
@@ -1,4 +1,16 @@
 return {
-  "alexghergh/nvim-tmux-navigation",
-  config = function() end,
+  {
+    "alexghergh/nvim-tmux-navigation",
+    config = function()
+      local nvim_tmux_nav = require("nvim-tmux-navigation")
+      nvim_tmux_nav.setup({
+        disable_when_zoomed = true, -- defaults to false
+      })
+
+      vim.keymap.set("n", "<C-h>", nvim_tmux_nav.NvimTmuxNavigateLeft, { desc = "Tmux Navigate Left" })
+      vim.keymap.set("n", "<C-j>", nvim_tmux_nav.NvimTmuxNavigateDown, { desc = "Tmux Navigate Down" })
+      vim.keymap.set("n", "<C-k>", nvim_tmux_nav.NvimTmuxNavigateUp, { desc = "Tmux Navigate Up" })
+      vim.keymap.set("n", "<C-l>", nvim_tmux_nav.NvimTmuxNavigateRight, { desc = "Tmux Navigate Right" })
+    end,
+  },
 }
