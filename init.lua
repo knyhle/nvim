d026f6b7c9cc437cec3022028fa78b8e9bdf859c
Kenny Le <kennyle1412@gmail.com>
2024-02-27 19:15:41 -0600

    feat: add python host prog

--- init.lua
+++ init.lua
@@ -70,6 +70,14 @@ vim.opt.fillchars = {
 	eob = " ",
 }
 
+if vim.fn.has("win32") == 1 or vim.fn.has("win64") == 1 then
+	-- Set Python host for Windows
+	vim.g.python3_host_prog = "~/.pyenv-win-venv/envs/nvim/Scripts/python.exe"
+else
+	-- Set Python host for Unix-based systems
+	vim.g.python3_host_prog = "~/.pyenv/versions/nvim/bin/python"
+end
+
 -- Preview substitutions live, as you type!
 vim.opt.inccommand = "split"
 
@@ -774,7 +782,7 @@ require("lazy").setup({
 		end,
 	},
 
-  {  import = "custom.plugins" }
+	{ import = "custom.plugins" },
 })
 
 -- The line beneath this is called `modeline`. See `:help modeline`
