b582ee4e97be4298c2d3df6381c7af847a89ad84
Kenny Le <kennyle1412@gmail.com>
2024-03-03 00:17:52 -0600

    chore: clean up some comments

--- init.lua
+++ init.lua
@@ -679,12 +679,9 @@ require("lazy").setup({
 					["<C-y>"] = cmp.mapping(function(fallback)
 						if cmp.visible() then
 							-- You could replace select_next_item() with confirm({ select = true }) to get VS Code autocompletion behavior
-							-- cmp.select_next_item()
 							cmp.confirm({ select = true })
 						-- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
 						-- this way you will only jump inside the snippet region
-						elseif luasnip.expand_or_locally_jumpable() then
-							luasnip.expand_or_jump()
 						else
 							fallback()
 						end
