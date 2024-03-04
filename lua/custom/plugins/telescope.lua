9cfa815d1e15460c94e0b0ab615aa2b5d906e32f
Kenny Le <kennyle1412@gmail.com>
2024-03-04 00:07:54 -0600

    feat: add dir search with <C-.>

--- lua/custom/plugins/telescope.lua
+++ lua/custom/plugins/telescope.lua
@@ -45,6 +45,15 @@ return {
 				}))
 			end, { desc = "[S]earch [F]iles" })
 
+			vim.keymap.set("n", "<C-.>", function()
+				builtin.find_files(require("telescope.themes").get_dropdown({
+					prompt_title = "Find Directories",
+					winblend = 10,
+					previewer = false,
+					find_command = { "fd", "--type", "d" },
+				}))
+			end, { desc = "[S]earch Directories" })
+
 			vim.keymap.set("n", "<leader>sw", builtin.grep_string, { desc = "[S]earch current [W]ord" })
 			vim.keymap.set("n", "<leader>sg", builtin.live_grep, { desc = "[S]earch by [G]rep" })
 			vim.keymap.set("n", "<leader>sd", builtin.diagnostics, { desc = "[S]earch [D]iagnostics" })
