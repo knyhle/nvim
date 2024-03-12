3ba27dafa2da639e9cbf8f3b6c472cc3a6dae1ab
Kenny Le <kennyle1412@gmail.com>
2024-03-11 23:58:13 -0500

    feat: update plugins and text object context

--- lua/custom/plugins/mini.lua
+++ lua/custom/plugins/mini.lua
@@ -10,19 +10,7 @@ return {
 			--  - va)  - [V]isually select [A]round [)]parenthen
 			--  - yinq - [Y]ank [I]nside [N]ext [']quote
 			--  - ci'  - [C]hange [I]nside [']quote
-			local ai = require("mini.ai")
-			require("mini.ai").setup({
-				n_lines = 500,
-				custom_textobjects = {
-					o = ai.gen_spec.treesitter({
-						a = { "@block.outer", "@conditional.outer", "@loop.outer" },
-						i = { "@block.inner", "@conditional.inner", "@loop.inner" },
-					}, {}),
-					f = ai.gen_spec.treesitter({ a = "@function.outer", i = "@function.inner" }, {}),
-					c = ai.gen_spec.treesitter({ a = "@class.outer", i = "@class.inner" }, {}),
-					t = { "<([%p%w]-)%f[^<%w][^<>]->.-</%1>", "^<.->().*()</[^/]->$" },
-				},
-			})
+			require("mini.ai").setup()
 
 			-- Add/delete/replace surroundings (brackets, quotes, etc.)
 			--
--- lua/custom/plugins/treesitter.lua
+++ lua/custom/plugins/treesitter.lua
@@ -3,18 +3,10 @@ return {
 	{ -- Highlight, edit, and navigate code
 		"nvim-treesitter/nvim-treesitter",
 		build = ":TSUpdate",
-		event = { "BufReadPre", "BufNewFile" },
 		dependencies = {
 			{ "nvim-treesitter/nvim-treesitter-textobjects" },
 		},
 		textobjects = {
-			move = {
-				enable = true,
-				goto_next_start = { ["]f"] = "@function.outer", ["]c"] = "@class.outer" },
-				goto_next_end = { ["]F"] = "@function.outer", ["]C"] = "@class.outer" },
-				goto_previous_start = { ["[f"] = "@function.outer", ["[c"] = "@class.outer" },
-				goto_previous_end = { ["[F"] = "@function.outer", ["[C"] = "@class.outer" },
-			},
 			select = {
 				enable = true,
 				lookahead = true,
