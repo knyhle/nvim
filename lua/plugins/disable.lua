9345351a8d065f5454ab7212c9f51a810847d817
Kenny Le <kennyle1412@gmail.com>
2024-07-01 18:24:01 -0500

    updsate

--- lua/plugins/disable.lua
+++ lua/plugins/disable.lua
@@ -1,16 +1,7 @@
 return {
-  -- {
-  --   "nvim-treesitter/nvim-treesitter-context",
-  --   enabled = false,
-  -- },
-  {
-    "nvim-neo-tree/neo-tree.nvim",
-    enabled = false,
-  },
-  {
-    "lukas-reineke/indent-blankline.nvim",
-    enabled = false,
-  },
+  { "folke/tokyonight.nvim", enabled = false },
+  { "nvim-neo-tree/neo-tree.nvim", enabled = false },
+  { "lukas-reineke/indent-blankline.nvim", enabled = false },
   {
     "echasnovski/mini.indentscope",
     enabled = false,
@@ -22,10 +13,7 @@ return {
       },
     },
   },
-  {
-    "RRethy/vim-illuminate",
-    enabled = false,
-  },
+  { "RRethy/vim-illuminate", enabled = false },
   {
     "akinsho/bufferline.nvim",
     enabled = false,
@@ -42,44 +30,7 @@ return {
       },
     },
   },
-  {
-    "echasnovski/mini.pairs",
-    enabled = false,
-  },
-  {
-    "folke/which-key.nvim",
-    enabled = false,
-  },
-  -- {
-  --   -- "numiras/semshi",
-  --   "wookayin/semshi", -- use a maintained fork
-  --   ft = "python",
-  --   build = ":UpdateRemotePlugins",
-  --   init = function()
-  --     -- Disabled these features better provided by LSP or other more general plugins
-  --     vim.g["semshi#error_sign"] = false
-  --     vim.g["semshi#simplify_markup"] = false
-  --     vim.g["semshi#mark_selected_nodes"] = false
-  --     vim.g["semshi#update_delay_factor"] = 0.001
-  --
-  --     -- This autocmd must be defined in init to take effect
-  --     vim.api.nvim_create_autocmd({ "VimEnter", "ColorScheme" }, {
-  --       group = vim.api.nvim_create_augroup("SemanticHighlight", {}),
-  --       callback = function()
-  --         -- Only add style, inherit or link to the LSP's colors
-  --         vim.cmd([[
-  --           " highlight! semshiGlobal gui=italic
-  --           highlight! link semshiImported @none
-  --           highlight! link semshiParameter @lsp.type.parameter
-  --           highlight! link semshiParameterUnused DiagnosticUnnecessary
-  --           highlight! link semshiBuiltin @function.builtin
-  --           highlight! link semshiAttribute @field
-  --           highlight! link semshiSelf @lsp.type.selfKeyword
-  --           highlight! link semshiUnresolved @lsp.type.unresolvedReference
-  --           highlight! link semshiFree @none
-  --           ]])
-  --       end,
-  --     })
-  --   end,
-  -- },
+  { "echasnovski/mini.pairs", enabled = false },
+  { "folke/which-key.nvim", enabled = false },
+  { "folke/flash.nvim", enabled = false },
 }
