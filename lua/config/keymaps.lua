740e33ba08db36798171092b5602077a2825245f
Kenny Le <kennyle1412@gmail.com>
2024-08-31 22:42:16 -0500

    feat: add tmux navigation

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -26,3 +26,10 @@ end)
 vim.keymap.set("n", "<S-l>", function()
   harpoon:list():next()
 end)
+
+local nvim_tmux_nav = require("nvim-tmux-navigation")
+
+vim.keymap.set("n", "<C-h>", nvim_tmux_nav.NvimTmuxNavigateLeft)
+vim.keymap.set("n", "<C-j>", nvim_tmux_nav.NvimTmuxNavigateDown)
+vim.keymap.set("n", "<C-k>", nvim_tmux_nav.NvimTmuxNavigateUp)
+vim.keymap.set("n", "<C-l>", nvim_tmux_nav.NvimTmuxNavigateRight)
