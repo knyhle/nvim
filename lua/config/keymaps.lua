821582b3233f4a5ed44658c7855711105c5925b7
Kenny Le <kennyle1412@gmail.com>
2024-09-01 23:43:04 -0500

    feat: update keymap and mini surround

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -33,3 +33,7 @@ vim.keymap.set("n", "<C-h>", nvim_tmux_nav.NvimTmuxNavigateLeft)
 vim.keymap.set("n", "<C-j>", nvim_tmux_nav.NvimTmuxNavigateDown)
 vim.keymap.set("n", "<C-k>", nvim_tmux_nav.NvimTmuxNavigateUp)
 vim.keymap.set("n", "<C-l>", nvim_tmux_nav.NvimTmuxNavigateRight)
+
+vim.keymap.set("n", "s", "<nop>", { silent = true })
+
+vim.keymap.set("n", "<leader>ya", "<CMD>%y<CR>", { desc = "Yank all" })
--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -20,6 +20,24 @@ return {
       opts.experimental = {
         ghost_text = {},
       }
+      opts.preselect = cmp.PreselectMode.None
+      opts.completion = {
+        completeopt = "menu,menuone,noinsert,noselect",
+      }
     end,
+  },
+  {
+    "echasnovski/mini.surround",
+    opts = {
+      mappings = {
+        add = "sa", -- Add surrounding in Normal and Visual modes
+        delete = "sd", -- Delete surrounding
+        find = "sf", -- Find surrounding (to the right)
+        find_left = "sF", -- Find surrounding (to the left)
+        highlight = "sh", -- Highlight surrounding
+        replace = "sr", -- Replace surrounding
+        update_n_lines = "sn", -- Update `n_lines`
+      },
+    },
   },
 }
