1408c3bf5131b5b909c3afcd74776691ba543581
Kenny Le <kenny.le@gm.com>
2024-09-06 16:46:59 -0500

    feat: edit keymaps

---
+++ lua/custom/plugins/quickfix.lua
@@ -0,0 +1,23 @@
+return {
+	{
+		"kevinhwang91/nvim-bqf",
+		enabled = false,
+		config = function()
+			require("bqf").setup()
+		end,
+	},
+	{
+		"stevearc/qf_helper.nvim",
+		opts = {},
+		config = function()
+			-- use <C-N> and <C-P> for next/prev.
+			require("qf_helper").setup()
+
+			vim.keymap.set("n", "<C-N>", "<CMD>QNext<CR>")
+			vim.keymap.set("n", "<C-P>", "<CMD>QPrev<CR>")
+			-- toggle the quickfix open/closed without jumping to it
+			vim.keymap.set("n", "<leader>q", "<CMD>QFToggle!<CR>")
+			vim.keymap.set("n", "<leader>l", "<CMD>LLToggle!<CR>")
+		end,
+	},
+}
