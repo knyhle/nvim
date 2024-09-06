50b65ef64de82f31fe5ef7e084b9a8a6ebb04987
Kenny Le <kenny.le@gm.com>
2024-09-06 14:30:16 -0500

    feat: add files based on kickstart

---
+++ plugin/options.lua
@@ -0,0 +1,35 @@
+-- [[ Setting options ]]
+-- Set to true if you have a Nerd Font installed and selected in the terminal
+local opt = vim.opt
+
+opt.colorcolumn = "88"
+opt.number = true
+opt.relativenumber = true
+opt.mouse = "a"
+opt.showmode = false
+vim.schedule(function()
+	opt.clipboard = "unnamedplus"
+end)
+opt.breakindent = true
+opt.undofile = true
+opt.ignorecase = true
+opt.smartcase = true
+opt.signcolumn = "yes"
+opt.updatetime = 250
+opt.timeoutlen = 300
+opt.splitright = true
+opt.splitbelow = true
+opt.list = true
+opt.inccommand = "split"
+opt.pumblend = 10 -- Popup blend
+opt.pumheight = 10 -- Maximum number of entries in a popup
+opt.cursorline = true
+opt.scrolloff = 10
+opt.tabstop = 4
+opt.shiftwidth = 4
+opt.smartindent = true
+opt.smartcase = true
+opt.splitkeep = "screen"
+opt.autowrite = true
+
+opt.guicursor = { "n-v-i-c:block", "ci-ve:ver25", "r-cr:hor20", "o:hor50", "a:blinkon100" }
