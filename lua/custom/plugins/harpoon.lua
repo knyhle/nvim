092c97a2a9066dc716730727d87dd0d59863cb23
Kenny Le <kenny.le@gm.com>
2024-09-06 16:23:35 -0500

    feat: add git signs

--- lua/custom/plugins/harpoon.lua
+++ lua/custom/plugins/harpoon.lua
@@ -13,14 +13,14 @@ return {
 		keys = function()
 			local keys = {
 				{
-					"<M-h>",
+					"<M-a>",
 					function()
 						require("harpoon"):list():add()
 					end,
 					desc = "Harpoon File",
 				},
 				{
-					"<leader>h",
+					"<M-h>",
 					function()
 						local harpoon = require("harpoon")
 						harpoon.ui:toggle_quick_menu(harpoon:list())
