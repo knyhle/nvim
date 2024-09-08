2d56c92f400dd94b80e4c4447b8b34149d9be828
Kenny Le <kenny.le@gm.com>
2024-09-08 00:09:41 -0500

    feat: remove some unused plugins

--- lua/custom/plugins/color.lua
+++ lua/custom/plugins/color.lua
@@ -13,14 +13,4 @@ return {
 			},
 		},
 	},
-	{
-		"folke/tokyonight.nvim",
-		priority = 1000, -- Make sure to load this before all the other start plugins.
-		opts = {
-			styles = {
-				comments = { italic = false },
-				keywords = { italic = false },
-			},
-		},
-	},
 }
