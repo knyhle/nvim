d9d3adefc82e3ba8dedb0dfdca8ef3fd7623e12c
Kenny Le <kennyle1412@gmail.com>
2024-09-11 23:37:46 -0500

    feat: add golang lsp

--- lua/custom/plugins/conform.lua
+++ lua/custom/plugins/conform.lua
@@ -33,6 +33,8 @@ return { -- Autoformat
     end,
     formatters_by_ft = {
       lua = { "stylua" },
+      go = { "goimports", "gofumpt" },
+
       -- Conform can also run multiple formatters sequentially
       -- python = { "isort", "black" },
       --
