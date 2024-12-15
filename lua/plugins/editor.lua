4ec739085eb70c0b601b8da77d12bdef04b6a790
Kenny Le <kennyle1412@gmail.com>
2024-12-15 01:02:15 -0600

    add colorscheme

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -1,4 +1,20 @@
 return {
+    {
+        "catppuccin/nvim",
+        name = "catppuccin",
+        priority = 1000,
+        config = function ()
+            require("catppuccin").setup({
+                flavour = "macchiato",
+                transparent_background = true,
+                term_colors = true,
+                no_italic = true,
+            })
+
+            vim.cmd.colorscheme "catppuccin"
+        end
+    },
+
     -- "gc" to comment visual regions/lines
     { 'numToStr/Comment.nvim', opts = {} },
 
