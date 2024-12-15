27e98f505f0e357a6c919b42f6f3130b74ab8576
Kenny Le <kennyle1412@gmail.com>
2024-12-15 01:02:08 -0600

    feat: Modify plugin settings and add new plugins
    - Disable animation in 'edgy.nvim' and add 'CopilotChat.nvim' window configuration
    - Change animation function in 'mini.indentscope'
    - Add new plugins 'noice.nvim' and 'nvim-notify' with initial configurations

--- lua/plugins/edgy.lua
+++ lua/plugins/edgy.lua
@@ -3,7 +3,14 @@ return {
     "folke/edgy.nvim",
     opts = {
       animate = {
-        enable = true,
+        enabled = false,
+      },
+      right = {
+        {
+          title = "CopilotChat.nvim", -- Title of the window
+          ft = "copilot-chat", -- This is custom file type from CopilotChat.nvim
+          size = { width = 0.4 }, -- Width of the window
+        },
       },
     },
   },
---
+++ lua/plugins/notify.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "rcarriga/nvim-notify",
+    opts = {
+      render = "compact",
+      stages = "static",
+      timeout = 3000,
+      top_down = false,
+    },
+  },
+}
