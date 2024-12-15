c362bebf932be3efcb792a899cec19a1dc2d572a
Kenny Le <kennyle1412@gmail.com>
2024-12-15 01:02:08 -0600

    update neovim

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,4 +2,10 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
-vim.g.python3_host_prog = "~/.pyenv/versions/nvim/bin/python"
+if vim.fn.has("win32") == 1 or vim.fn.has("win64") == 1 then
+  -- Set Python host for Windows
+  vim.g.python3_host_prog = "~/.pyenv-win-venv/envs/nvim/Scripts/python.exe"
+else
+  -- Set Python host for Unix-based systems
+  vim.g.python3_host_prog = "~/.pyenv/versions/nvim/bin/python"
+end
