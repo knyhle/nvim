875875b63a36810f581f989c64392ee818724008
Kenny Le <kennyle1412@gmail.com>
2024-12-15 01:02:14 -0600

    update oil and add treesitter configs

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -6,5 +6,61 @@ return {
             'nvim-treesitter/nvim-treesitter-textobjects',
         },
         build = ':TSUpdate',
-    }
+        ---@type TSConfig
+        ---@diagnostic disable-next-line: missing-fields
+        opts = {
+            highlight = { enable = true },
+            indent = { enable = true },
+            ensure_installed = {
+                "bash",
+                "c",
+                "html",
+                "javascript",
+                "json",
+                "lua",
+                "luadoc",
+                "luap",
+                "markdown",
+                "markdown_inline",
+                "python",
+                "tsx",
+                "typescript",
+                "vim",
+                "vimdoc",
+                "yaml",
+            },
+            textobjects = {
+                move = {
+                    enable = true,
+                    goto_next_start = { ["]f"] = "@function.outer", ["]c"] = "@class.outer" },
+                    goto_next_end = { ["]F"] = "@function.outer", ["]C"] = "@class.outer" },
+                    goto_previous_start = { ["[f"] = "@function.outer", ["[c"] = "@class.outer" },
+                    goto_previous_end = { ["[F"] = "@function.outer", ["[C"] = "@class.outer" },
+                },
+            },
+        },
+        ---@param opts TSConfig
+        config = function(_, opts)
+            if type(opts.ensure_installed) == "table" then
+                ---@type table<string, boolean>
+                local added = {}
+                opts.ensure_installed = vim.tbl_filter(function(lang)
+                    if added[lang] then
+                        return false
+                    end
+                    added[lang] = true
+                    return true
+                end, opts.ensure_installed)
+            end
+            require("nvim-treesitter.configs").setup(opts)
+        end,
+    },
+
+    -- Show context of the current function
+    -- {
+    --     "nvim-treesitter/nvim-treesitter-context",
+    --     event = "VeryLazy",
+    --     enabled = false,
+    --     opts = { mode = "cursor", max_lines = 3 },
+    -- },
 }
