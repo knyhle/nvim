b03746760e8a882b383eeff7769370f4ce10ba83
Kenny Le <kennyle1412@gmail.com>
2024-12-15 01:02:12 -0600

    feat: Enhance Harpoon and Supertab plugins
    - Refactor Harpoon keymap set functions for better readability
    - Add new Supertab plugin with LuaSnip and nvim-cmp configurations

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -15,13 +15,25 @@ return {
       harpoon.setup()
 
       vim.keymap.set("n", "<leader>fm", "<cmd>Telescope harpoon marks<cr>", { desc = "Telescope Harpoon Marks" })
-      vim.keymap.set("n", "<leader>ha", function() harpoon:list():append() end)
-      vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+      vim.keymap.set("n", "<leader>a", function()
+        harpoon:list():append()
+      end)
+      vim.keymap.set("n", "<C-e>", function()
+        harpoon.ui:toggle_quick_menu(harpoon:list())
+      end)
 
-      vim.keymap.set("n", "<leader>hh", function() harpoon:list():select(1) end)
-      vim.keymap.set("n", "<leader>ht", function() harpoon:list():select(2) end)
-      vim.keymap.set("n", "<leader>hn", function() harpoon:list():select(3) end)
-      vim.keymap.set("n", "<leader>hs", function() harpoon:list():select(4) end)
+      vim.keymap.set("n", "<leader>hh", function()
+        harpoon:list():select(1)
+      end)
+      vim.keymap.set("n", "<leader>ht", function()
+        harpoon:list():select(2)
+      end)
+      vim.keymap.set("n", "<leader>hn", function()
+        harpoon:list():select(3)
+      end)
+      vim.keymap.set("n", "<leader>hs", function()
+        harpoon:list():select(4)
+      end)
 
       local tele_status_ok, telescope = pcall(require, "telescope")
       if not tele_status_ok then
@@ -32,4 +44,3 @@ return {
     end,
   },
 }
-
