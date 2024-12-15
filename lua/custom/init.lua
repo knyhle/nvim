128b6940d02f5c70e0aded57228d1c6562c73b0d
Kenny Le <kennyle1412@gmail.com>
2024-12-15 01:02:14 -0600

    add some more plugins. mostly fzf

--- lua/custom/init.lua
+++ lua/custom/init.lua
@@ -1,3 +1,43 @@
 require("custom.options")
 require("custom.mapping")
 require("custom.autocmds")
+
+vim.defer_fn(function()
+    require('nvim-treesitter.configs').setup {
+        -- Add languages to be installed here that you want installed for treesitter
+        ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'javascript', 'typescript', 'vimdoc', 'vim', 'bash' },
+
+        -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
+        auto_install = true,
+        -- Install languages synchronously (only applied to `ensure_installed`)
+        sync_install = false,
+        -- List of parsers to ignore installing
+        ignore_install = {},
+        -- You can specify additional Treesitter modules here: -- For example: -- playground = {--enable = true,-- },
+        modules = {},
+        highlight = { enable = true },
+        indent = { enable = true },
+        textobjects = {
+            select = {
+                enable = true,
+                lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
+                keymaps = {
+                    -- You can use the capture groups defined in textobjects.scm
+                    ['aa'] = '@parameter.outer',
+                    ['ia'] = '@parameter.inner',
+                    ['af'] = '@function.outer',
+                    ['if'] = '@function.inner',
+                    ['ac'] = '@class.outer',
+                    ['ic'] = '@class.inner',
+                },
+                move = {
+                    enable = true,
+                    goto_next_start = { ["]f"] = "@function.outer", ["]c"] = "@class.outer" },
+                    goto_next_end = { ["]F"] = "@function.outer", ["]C"] = "@class.outer" },
+                    goto_previous_start = { ["[f"] = "@function.outer", ["[c"] = "@class.outer" },
+                    goto_previous_end = { ["[F"] = "@function.outer", ["[C"] = "@class.outer" },
+                },
+            },
+        },
+    }
+end, 0)
