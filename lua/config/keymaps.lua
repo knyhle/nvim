c8f4e5a66f365a968af7bfa4ad7497383412cbe8
Kenny Le <kennyle1412@gmail.com>
2024-12-15 01:02:08 -0600

    feat: Improve keymap settings and plugin configurations
    - Refactor keymap settings for better usability
    - Rename functions to follow camelCase convention
    - Update color scheme in lazy.lua
    - Remove copilot-chat.lua and move its content to copilot.lua
    - Add new plugins: edgy.nvim and mini.indentscope
    - Remove unnecessary code and comments for cleaner codebase

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,27 +2,19 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-vim.keymap.set("n", ";", ":", { desc = "command" })
-vim.keymap.set("n", ":", ";", { desc = "command" })
+vim.keymap.set({ "n", "v", "x" }, ";", ":", { desc = "command" })
+vim.keymap.set({ "n", "v", "x" }, ":", ";", { desc = "command" })
 
 vim.keymap.set("x", "<leader>p", '"_dP', { desc = "Paste without overwriting clipboard" })
 
-function Jump_and_center()
+function JumpAndCenter()
   local char = vim.fn.nr2char(vim.fn.getchar())
   vim.cmd("normal! '" .. char)
   vim.cmd("normal! zz")
 end
 
-vim.api.nvim_set_keymap("n", "'", ":lua Jump_and_center()<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "`", ":lua Jump_and_center()<CR>", { noremap = true, silent = true })
-
-function SaveAndCenter()
-  vim.cmd("write")
-  vim.cmd("normal! zz")
-end
-
--- vim.keymap.set({ "n", "i", "v" }, "<C-s>", "<cmd>w<CR>", { desc = "Save File" })
-vim.keymap.set({ "n", "i", "v" }, "<C-s>", ":lua SaveAndCenter()<CR>", { desc = "Save File" })
+vim.api.nvim_set_keymap("n", "'", ":lua JumpAndCenter()<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "`", ":lua JumpAndCenter()<CR>", { noremap = true, silent = true })
 
 vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "Scroll down" })
 vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "Scroll up" })
