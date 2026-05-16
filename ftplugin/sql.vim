5f68eb46f813312c06a5732d0d0eeaca1ac6c984
Kenny Le <khl455@eid.utexas.edu>
2026-05-16 00:28:11 -0500

    reset kickstart

--- ftplugin/sql.vim
+++
@@ -1,7 +0,0 @@
-
-set tabstop=4
-set shiftwidth=4
-set smarttab
-set expandtab
-let g:omni_sql_no_default_maps = 1
-
--- lua/custom/plugins/lualine.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "nvim-lualine/lualine.nvim",
-  opts = {
-    options = {
-      theme = "catppuccin",
-    },
-  },
-  dependencies = { "nvim-tree/nvim-web-devicons" },
-}
